b1f64047c4700949d8e8846158eadc98e9992172
Diego Belli <128156867+diego-belli7@users.noreply.github.com>
2024-06-10 09:05:02 -0300

    Add files via upload

---
+++ Aula01/objetos.js
@@ -0,0 +1,8 @@
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "11122233344"];
+
+const ojetopessoa = {
+    nome: "Diego",
+    idade: 16,
+    cpf:"11122233344",
+    email: "bellidiego254@gmail.com"
+}
---
+++ Aula02/tipo.js
@@ -0,0 +1,7 @@
+const cliente = {
+    nome: "Diego",
+    idade: 16,
+    email: "a.diego@escola.pr.gov.br",
+    telefone: ["43988379178", "43996032929"]
+};
+console.log;
